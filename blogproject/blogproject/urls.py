4cab50efea2961d68d5215423d541b57f6cc6fd9
arong12 <sungeunsungeun1018@likelion.org>
2022-05-29 13:48:14 +0900

    django very hard

---
+++ blogproject/blogproject/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from blogapp import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    #html form 이용해 블로그 객체 생성
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
+    path('detail/<int:blog_id>', views.detail, name='detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    #media 파일에 접근할 수 있는 url도 추가해야 함
